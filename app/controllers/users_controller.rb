a5c8a932fca03a537958c9a6306678bfce7ca9f0
Shun Yamazaki <scarecrow.gang.duck@gmail.com>
2019-03-30 11:31:39 +0900

    Add a form of Users Registoration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcom to the Sample App!'
+      redirect_to @user # redirect_to user_path(@user)の省略形(GET /users/user[:id])
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: {
+        user: {
+          name: '',
+          email: 'user@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+
+    assert_template 'users/new'
+    assert_select 'form[action=?]', signup_path
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {
+        user: {
+          name: 'Test User',
+          email: 'user@test.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
+  end
+end
